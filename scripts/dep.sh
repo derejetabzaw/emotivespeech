f7ad4054b19c7c9cd2d3ab46a4ec7dc04f813c17
Dereje <dergkat@yahoo.ca>
2017-06-13 20:31:39 +0300

    Install Deps

--- scripts/dep.sh
+++ scripts/dep.sh
@@ -3,6 +3,7 @@
 # dev.sh
 #
 # This script will install dependecies for the emotive_sppech
+# 
 echo "Library Dependecies..."
 sleep 3
 echo "Installing pysox-Python Wrapper for the SoX-Sound Exchange"
@@ -10,10 +11,8 @@ sudo pip install sox
 echo "Finished Installing pysox"
 sleep 2
 echo "Installing pysptk-Python Wrapper for Speech Processing Tool Kit"
-mkdir lib
-cd lib
-git clone https://github.com/r9y9/pysptk
-cd pysptk
-sudo python setup.py install 
+echo "Make Sure Python --version is:Python >= 2.7.9"
+echo "Otherwise the pysptk will not be installed with pip"
+sudo pip install pysptk
 echo "Done Installing pysptk"
 
