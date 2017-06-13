ae7c70c8e4326a323a4df67aadb539264f04dc22
Dereje <dergkat@yahoo.ca>
2017-06-13 20:11:39 +0300

    Install Deps

--- scripts/dep.sh
+++ scripts/dep.sh
@@ -10,10 +10,7 @@ sudo pip install sox
 echo "Finished Installing pysox"
 sleep 2
 echo "Installing pysptk-Python Wrapper for Speech Processing Tool Kit"
-mkdir -p pysptk
-cd ..
-cd pysptk
-git clone https://github.com/r9y9/pysptk
-python setup.py install 
+cd lib/pysptk
+sudo python setup.py install 
 echo "Done Installing pysptk"
 
